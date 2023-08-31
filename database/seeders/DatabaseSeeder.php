53b9f621336fb128cc76b8618d3adb3b95878969
azizbek-tursunov <azizbektursunovofficial@gmail.com>
2023-08-31 15:09:04 +0500

    1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TimeSeeder::class,
             CategoriesSeeder::class,
-            UserSeeder::class,
+//            UserSeeder::class,
         ]);
     }
 }
